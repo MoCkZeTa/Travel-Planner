b735e084244a81bb13bd4510a622dc7821c6a97d
MoCkZeTa <ankittheoriginal1@gmail.com>
2025-07-03 22:58:48 +0530

    fix 10

--- app/trips/[tripId]/itinerary/new/page.tsx
+++ app/trips/[tripId]/itinerary/new/page.tsx
@@ -1,39 +1,121 @@
-import { addLocation } from "@/lib/actions/add-location";
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function NewLocation({ params }: { params: { tripId: string } }) {
-  const { tripId } = params;
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createTrip } from "@/lib/actions/create-trip";
+import { cn } from "@/lib/utils";
+import { UploadButton } from "@/lib/upload-thing";
+import { useState, useTransition } from "react";
+import Image from "next/image";
+
+export default function NewTrip() {
+  const [isPending, startTransition] = useTransition();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
   return (
-    <div className="min-h-[calc(100vh-8rem)] flex items-center justify-center bg-gray-50">
-      <div className="w-full max-w-md mx-auto">
-        <div className="bg-white p-8 shadow-lg rounded-lg">
-          <h1 className="text-3xl font-bold text-center mb-6">
-            Add New Location
-          </h1>
+    <div className="max-w-lg mx-auto mt-10">
+      <Card>
+        <CardHeader> New Trip</CardHeader>
+        <CardContent>
           <form
             className="space-y-6"
-            action={async (formData) => {
-              "use server";
-              await addLocation(formData, tripId);
+            action={(formData: FormData) => {
+              if (imageUrl) {
+                formData.append("imageUrl", imageUrl);
+              }
+              startTransition(() => {
+                createTrip(formData);
+              });
             }}
           >
             <div>
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                Address
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                {" "}
+                Title
               </label>
               <input
-                name="address"
                 type="text"
+                name="title"
+                placeholder="Japan trip..."
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
                 required
-                className="w-full border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <Button type="submit" className="w-full">
-              Add Location
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Description
+              </label>
+              <textarea
+                name="description"
+                placeholder="Trip description..."
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+                required
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Start Date
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  {" "}
+                  End Date
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+            </div>
+            <div>
+              <label> Trip Image</label>
+
+              {imageUrl && (
+                <Image
+                  src={imageUrl}
+                  alt="Trip Preview"
+                  className="w-full mb-4 rounded-md max-h-48 object-cover"
+                  width={300}
+                  height={100}
+                />
+              )}
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  if (res && res[0].ufsUrl) {
+                    setImageUrl(res[0].ufsUrl);
+                  }
+                }}
+                onUploadError={(error: Error) => {
+                  console.error("Upload error: ", error);
+                }}
+              />
+            </div>
+            <Button type="submit" disabled={isPending} className="w-full">
+              {isPending ? "Creating..." : "Create Trip"}
             </Button>
           </form>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
